8176ebb2a85e11581f573c11f3756e04a9e45528
dalogrus <sburek+github@gmail.com>
2015-03-23 21:22:37 +0100

    first rev of project

---
+++ SlotMachine/Slot.swift
@@ -0,0 +1,16 @@
+//
+//  Slot.swift
+//  SlotMachine
+//
+//  Created by Sebastian Burek on 09.03.2015.
+//  Copyright (c) 2015 Sebastian Burek. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Slot {
+    var value = 0
+    var image = UIImage(named: "Ace")
+    var isRed = true
+}
